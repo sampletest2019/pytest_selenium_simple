699086d26f89484a5840be27e2fbb93f1f2e637c
lrnx2018 <yuriy@learnixcenter.com>
2020-05-11 09:19:02 -0400

    chromedriver updated to 81.

--- amazon/test_search.py
+++ amazon/test_search.py
@@ -5,24 +5,26 @@ expected_title = 'Amazon.com: Online Shopping for Electronics, Apparel, Computer
 base_url = 'https://www.amazon.com'
 search_title = 'Amazon.com: nike air max'
 
+
 @pytest.fixture()
 def env_setup():
     global driver
     global baseUrl
     # we will use Google Chrome in this test. Specify the location of your chromedriver.exe
-    driver = webdriver.Chrome("C:\\Users\\chromedriver.exe")
-    # wait 10 seconds till the website will open
-    driver.implicitly_wait(10)
+    driver = webdriver.Chrome("../chromedriver_81.exe")
     # maximize browser window to full screen
     driver.maximize_window()
+    # pull the DOM for 10 seconds when trying to find any element
+    driver.implicitly_wait(10)
     yield
-    # when test is done, close ALL windows of the browser
+    # when test is done, close ALL windows of the browser which has been created
     driver.quit()
+
 
 def test_search_airmax(env_setup):
     # navigate to Amazon.com home page
     driver.get(base_url)
-    # verify that website title is Amazon.com
+    # verify that web page title is Amazon.com
     assert driver.title == expected_title
     # locate search field element
     search_field = driver.find_element_by_id("twotabsearchtextbox")
@@ -30,7 +32,7 @@ def test_search_airmax(env_setup):
     search_field.send_keys("nike air max")
     # locate search button
     search_button = driver.find_element_by_xpath("//input[@value='Go']")
-    # click on 'Search' icon
+    # click on 'Search' button
     search_button.click()
-    # verify the page title
+    # verify actual page title matches expected page title
     assert driver.title == search_title
