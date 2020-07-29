24103dcc33943d72e42f6648342b12f4bf305f3b
Yury Kolomiytsev <yuriyfromrussia@hotmail.com>
2020-07-29 16:13:48 -0400

    conftest added. ChromeDriver platform OS auto detection added.

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import platform
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture()
+def browser():
+    chrome_version_win = "84"
+    chrome_version_mac = "84"
+    chrome_version_linux = "84"
+
+    if 'win' in platform.platform():
+        browser = webdriver.Chrome("../resources/chromedriver_win_{}.exe".format(chrome_version_win))
+    elif 'darwin' in platform.platform():
+        browser = webdriver.Chrome("../resources/chromedriver_mac_{}".format(chrome_version_mac))
+    elif 'macOS' in platform.platform():
+        browser = webdriver.Chrome("../resources/chromedriver_mac_{}".format(chrome_version_mac))
+    elif "linux" in platform.platform():
+        browser = webdriver.Chrome("../resources/chromedriver_linux_{}".format(chrome_version_linux))
+    else:
+        raise Exception("chromedriver is not configured for your Operation System! "
+                        "Your Operating System is: {}".format(platform.platform()))
+
+    # wait 10 seconds to pull the DOM
+    browser.implicitly_wait(10)
+    # maximize browser window to full screen
+    browser.maximize_window()
+    yield browser
+    # when test is done, close ALL windows of the browser
+    browser.quit()
