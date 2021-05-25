a9339e18fb2313f8e60b4c888d5087d7b9e9a569
Yury K <yuriyfromrussia@hotmail.com>
2021-05-24 20:44:41 -0400

    natalya deleted.

--- tests/test_title.py
+++ tests/test_title.py
@@ -4,7 +4,7 @@ import pytest
 expected_title = 'Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more'
 base_url = 'https://www.amazon.com'
 search_title = 'Amazon.com: nike air max'
-natalya = ''
+
 
 @pytest.mark.smoketest
 def test_title(browser):
