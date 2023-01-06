ef7c245253c9f9573f2314ffbe4e5c9c5217f684
Catherine Grey <Kaaatrinakuz@gmail.com>
2023-01-05 19:17:11 -0500

    test_macys_toys_page.py updated to verify page title.

--- tests/test_macys_toys_page.py
+++ tests/test_macys_toys_page.py
@@ -5,3 +5,4 @@ import pytest
 def test_verify_macys_toys_title(browser):
     browser.get("https://www.macys.com")
     browser.find_element(By.LINK_TEXT, "Toys").click()
+    assert browser.title == "All Toys - Macy's"
