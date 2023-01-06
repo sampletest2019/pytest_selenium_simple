be8e2e59e3793252fcda6360762e4e007571d062
Catherine Grey <Kaaatrinakuz@gmail.com>
2023-01-05 19:05:59 -0500

    test_macys_toys_page.py added.

---
+++ tests/test_macys_toys_page.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.mark.smoketest
+def test_verify_macys_toys_title(browser):
+    browser.get("https://www.macys.com")
+    browser.find_element(By.LINK_TEXT, "Toys").click()
